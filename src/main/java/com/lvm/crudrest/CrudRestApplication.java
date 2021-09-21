5d57df3aa41e49338f58358f3c124722076c7b11
Luan Velasco <velascoluan@gmail.com>
2021-09-21 14:11:44 -0300

    initialize Data Base

--- src/main/java/com/lvm/crudrest/CrudRestApplication.java
+++ src/main/java/com/lvm/crudrest/CrudRestApplication.java
@@ -1,7 +1,14 @@
 package com.lvm.crudrest;
 
+import java.util.stream.LongStream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.lvm.crudrest.entity.Flights;
+import com.lvm.crudrest.repositories.FlightRepository;
 
 @SpringBootApplication
 public class CrudRestApplication {
@@ -9,5 +16,21 @@ public class CrudRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudRestApplication.class, args);
 	}
-
+	
+	@Bean
+    CommandLineRunner init(FlightRepository repository) {
+        return args -> {
+            repository.deleteAll();
+            LongStream.range(1, 11).mapToObj(i -> {
+                        Flights flights = new Flights();
+                        flights.setFlightCompany("Company " + i);
+                        flights.setAircraftModel("Boing-" + i + "-737");
+                        flights.setAircraftIdentificationCode("BRA-" + i);
+                        return flights;
+            	})
+                    .map(v -> repository.save(v))
+                    .forEach(System.out::println);
+        };
+    }
+	
 }
